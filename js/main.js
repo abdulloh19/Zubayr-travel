bd60ad8c625c727e8e2da93fb4fa7a7a06a680f1
abdulloh19 <parij4320@gmail.com>
2022-04-24 17:15:25 +0500

    alert add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// const $500 = Mehmonxona;
+// const $250 = Bilet;
+
+
+
 const wrapperValeu = document.querySelector(".wrapper");
 
 const usd = 11000;
@@ -5,11 +10,21 @@ const evro = 13000;
 const ticket = 250;
 const hotel = 500;
 
+
+
 const pulMiqdori = usd * hotel + evro * ticket;
 const xarajatlar = +prompt("O'zingizdagi pul miqdorini kiriting");
+
+alert(`
+    Mehmonxona xarajati uchun: $${hotel};
+    Bilet narhi uchun: $${ticket}
+`)
 
 if (pulMiqdori <= xarajatlar) {
     wrapperValeu.textContent = "Oq yo'l Alisher"
 } else {
     wrapperValeu.textContent = "Alisher ozgina sabr qilish kerak bo'lar ekan"
-}
+};
+
+
+
