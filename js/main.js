d306306eeb8ad336ee911fb3d300fe254f5f06f9
abdulloh19 <parij4320@gmail.com>
2022-04-25 14:39:00 +0500

    day work add

--- js/main.js
+++ js/main.js
@@ -1,28 +1,39 @@
-// const $500 = Mehmonxona;
-// const $250 = Bilet;
+// const wrapperValeu = document.querySelector(".wrapper");
+
+// const usd = 11000;
+// const evro = 13000;
+// const ticket = 250;
+// const hotel = 500;
+
+// alert(`
+//     Mehmonxona xarajati uchun: $${hotel};
+//     Bilet narhi uchun: $${ticket}
+// `)
+
+// const pulMiqdori = usd * hotel + evro * ticket;
+// const xarajatlar = +prompt("O'zingizdagi pul miqdorini kiriting");
+
+// if (pulMiqdori <= xarajatlar) {
+//     wrapperValeu.textContent = "Oq yo'l Zubayr"
+// } else {
+//     wrapperValeu.textContent = "Zubayr ozgina sabr qilish kerak bo'lar ekan"
+// };
+
+const sum = document.querySelector(".work")
+
+const wrapperConst = 1 && 2 && 3 && 4 && 5;
+const work = 6 && 7;
+
+const promptNumber = +prompt("1 dan 7 gacha bo'lgan sonni kiriting");
+
+if (wrapperConst >= promptNumber) {
+    sum.textContent = "Bugun sizning ish kuningiz"
+} else if (work >= promptNumber) {
+    sum.textContent = "Bugun sizning dam olish kuningiz"
+}
 
 
 
-const wrapperValeu = document.querySelector(".wrapper");
-
-const usd = 11000;
-const evro = 13000;
-const ticket = 250;
-const hotel = 500;
-
-alert(`
-    Mehmonxona xarajati uchun: $${hotel};
-    Bilet narhi uchun: $${ticket}
-`)
-
-const pulMiqdori = usd * hotel + evro * ticket;
-const xarajatlar = +prompt("O'zingizdagi pul miqdorini kiriting");
-
-if (pulMiqdori <= xarajatlar) {
-    wrapperValeu.textContent = "Oq yo'l Zubayr"
-} else {
-    wrapperValeu.textContent = "Zubayr ozgina sabr qilish kerak bo'lar ekan"
-};
 
 
 
